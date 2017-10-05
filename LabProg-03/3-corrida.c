c9c6bcdccfa3b995223df97073a344875b26bc75
Franklyn-S <franklynseabra12@hotmail.com>
2017-10-05 13:14:35 -0300

    3-questão questão solucionada!

--- LabProg-03/3-corrida.c
+++ LabProg-03/3-corrida.c
@@ -1,9 +1,9 @@
 # include <stdio.h>
 
 float melhorTempo(float* vetor, int n);
-float voltaMelhorTempo(float* vetor, int n);
+int voltaMelhorTempo(float* vetor, int n);
 float piorTempo(float* vetor, int n);
-float voltaPiorTempo(float* vetor, int n);
+int voltaPiorTempo(float* vetor, int n);
 
 
 int main(void){
@@ -17,23 +17,25 @@ int main(void){
 		printf("Digite o tempo registrado na volta %d:\n", i+1);
 		scanf("%f", &vet_temp[i]);
 	}
+
 	printf("Melhor tempo:%.2f\n", melhorTempo(vet_temp,n));
-	printf("Volta Melhor tempo:%f\n", voltaMelhorTempo(vet_temp,n));
+	printf("Volta Melhor tempo:%d\n", voltaMelhorTempo(vet_temp,n));
 	printf("Pior tempo:%.2f\n", piorTempo(vet_temp,n));
-	printf("Volta Pior tempo:%f\n", voltaPiorTempo(vet_temp,n));
+	printf("Volta Pior tempo:%d\n", voltaPiorTempo(vet_temp,n));
 	return 1;
 }
 
 float melhorTempo(float* vetor, int n){
 	float T = vetor[0];
 	int i;
-	for(i = 1; i < n; i++){
-		if (T > vetor[i]);
+	for(i = 0; i < n; i++){
+		if (T > vetor[i]){
 			T = vetor[i];
 		}
+	}
 	return T;
 }
-float voltaMelhorTempo(float* vetor, int n){
+int voltaMelhorTempo(float* vetor, int n){
 	int volta = 0;
 	float T = melhorTempo(vetor, n);
 	int i;
@@ -48,12 +50,13 @@ float piorTempo(float* vetor, int n){
 	float T = vetor[0];
 	int i;
 	for(i = 1; i < n; i++){
-		if (T < vetor[i]);
+		if (T < vetor[i]){
 			T = vetor[i];
 		}
+	}
 	return T;
 }
-float voltaPiorTempo(float* vetor, int n){
+int voltaPiorTempo(float* vetor, int n){
 	int volta = 0;
 	float T = piorTempo(vetor, n);
 	int i;
